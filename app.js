fa859ac2d4974d8430a4d9f635e0ff829f4ffe7f
Patrickngutz <106065698+Patrickngutz@users.noreply.github.com>
2022-09-30 18:53:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
-const menu = document.querySelector('#mobile-menu') 
-const menuLinks = document.querySelector('.navbar__menu')
-  
-menu.addEventListener('click', function() { 
- menu.classList.toggle('is-active'); 
- menuLinks.classList.toggle('active');    
-}); 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
+  /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction2() {
+  document.getElementById("myDropdown-not").classList.toggle("show-not");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn-not')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-not");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show-not')) {
+        openDropdown.classList.remove('show-not');
+      }
+    }
+  }
+}
+
+/* Pre-check checklist button*/
+
+ /* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction3() {
+  document.getElementById("myDropdown-req").classList.toggle("show-req");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn-req')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-req");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show-req')) {
+        openDropdown.classList.remove('show-req');
+      }
+    }
+  }
+}
